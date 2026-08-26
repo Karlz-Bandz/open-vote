bf664c777b5abb348855c21606b0a0b2a4ffd8cd
Karlz-Bandz <karol2646@gmail.com>
2026-08-26 14:54:12 +0200

    Init

---
+++ src/main/java/com/izzisoft/open_vote/OpenVoteApplication.java
@@ -0,0 +1,13 @@
+package com.izzisoft.open_vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenVoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izzisoft/open_vote/OpenVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzisoft.open_vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
